9313da6d33880bbbc2db0f5f2236b011390b6784
Harsh Singh <43901843+DankMemes4President@users.noreply.github.com>
2022-01-31 13:37:14 +0530

    Update settings.py

--- DVM_task3/settings.py
+++ DVM_task3/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = f'{django_secret_key}'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
